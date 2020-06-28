edf3bd03f55063ef0162412fa2e700ac565fc6a9
Bryan <bryanortt@gmail.com>
2020-06-28 15:46:46 -0700

    Completed Splash window

---
+++ app/src/main/java/com/example/quiztaker/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.quiztaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ProgressBar spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        spinner = (ProgressBar)findViewById(R.id.spinner);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                Intent i = new Intent(SplashScreen.this, Login.class); startActivity(i);
+                finish(); } }, 4000);
+    }
+}
