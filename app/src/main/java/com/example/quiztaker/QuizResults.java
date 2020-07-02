72c7bb540106913095e8056df5b3cd9cd844aefb
Eric <Twotex187@hotmail.com>
2020-07-02 09:06:52 -0700

    Almost there.

--- app/src/main/java/com/example/quiztaker/QuizResults.java
+++ app/src/main/java/com/example/quiztaker/QuizResults.java
@@ -8,12 +8,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class QuizResults extends AppCompatActivity {
     private TextView theScore;
@@ -26,7 +26,7 @@ public class QuizResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_results);
-        theScore = findViewById(R.id.detailedScoreTextView);
+        theScore = findViewById(R.id.quizResultsTextView);
         theQuestion = findViewById(R.id.theQuestionTextView);
         theAnswer = findViewById(R.id.theAnswerTextView);
         questionsListView = findViewById(R.id.questionNumberListView);
@@ -34,10 +34,6 @@ public class QuizResults extends AppCompatActivity {
 
         DataBaseHelper dataBaseHelper = new DataBaseHelper((getApplicationContext()));
 
-
-        //theScore.setText("Final Score: 7/10 - 70% - Grade: C"); //UPDATE THIS WITH QUERY
-
-        // MAKE THIS DATA SHOW ON SCREEN
         Intent incomingIntent = getIntent();
         String username = incomingIntent.getStringExtra("username");
         String quizId = incomingIntent.getStringExtra("quizId");
@@ -49,6 +45,7 @@ public class QuizResults extends AppCompatActivity {
         final ArrayList<String> questions = incomingIntent.getStringArrayListExtra("questions");
         // ALL THE DATA ABOVE IS COMING IN CORRECTLY
 
+        String theScoreString = correct + "/" + totalQuestions;
         System.out.println("Username: " + username);
         System.out.println("Quiz id: " + quizId);
         System.out.println("Correct: " + correct);
@@ -59,10 +56,12 @@ public class QuizResults extends AppCompatActivity {
         System.out.print("\n\n\n\n");
         System.out.println("Answers: " + solutions.toString());
 
+        findPercentage(theScoreString);
+        findGrade(theScoreString);
 
-        theScore.setText("Score for " + quizName + ": " + correct + "/" + totalQuestions);
+        theScore.setText("Grade: " + findGrade(theScoreString) + " - Percentage: " + findPercentage(theScoreString) + " - Score: " + theScoreString);
         quizNumOfQuestions = Integer.parseInt(totalQuestions);
-        dataBaseHelper.insertQuiz(username,category, quizId, theScore.getText().toString(), quizName);
+        dataBaseHelper.insertQuiz(username, category, quizId, theScoreString, quizName);
 
         // going into Database (username, category, quiz_id)
 
@@ -88,9 +87,61 @@ public class QuizResults extends AppCompatActivity {
 
                     theQuestion.setText(questions.get(position));
                     theAnswer.setText(solutions.get(position));
-//                }
             }
-    });
+        });
+    }
+
+    protected String findPercentage(String score) {
+        int numerator;
+        int denominator;
+
+        List<String> items = Arrays.asList(score.split("/"));
+        System.out.println("List Contents: " + items.toString());
+        numerator = Integer.parseInt(items.get(0));
+        denominator = Integer.parseInt(items.get(1));
+
+        double ratio = (double)numerator / (double)denominator;
+        ratio = ratio * 100;
+        String finalPercentage = String.format("%.2f%%", ratio);
+        return finalPercentage;
+    }
+
+    protected String findGrade(String score) {
+        int numerator;
+        int denominator;
+
+        List<String> items = Arrays.asList(score.split("/"));
+        System.out.println("List Contents: " + items.toString());
+        numerator = Integer.parseInt(items.get(0));
+        denominator = Integer.parseInt(items.get(1));
+
+        double ratio = (double)numerator / (double)denominator;
+        ratio = ratio * 100;
+        String finalGrade;
+
+        if (ratio >= 90.0) {
+            finalGrade = "A";
+        }
+
+        else if (ratio >= 80.0) {
+            finalGrade = "B";
+        }
+
+        else if (ratio >= 70.0) {
+            finalGrade = "C";
+        }
+
+        else if (ratio >= 60.0) {
+            finalGrade = "D";
+        }
+
+        else {
+            finalGrade = "F";
+        }
+
+        return finalGrade;
+    }
 }
-}
+
+
 
